f18de8e4c83e01d92692b1af81032dc0760fe6d0
Paul Tobin <tobster111@gmail.com>
2019-04-13 17:54:46 +0100

    Added a renderList function to map over the object

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class SongList extends Component {
+  renderList() {
+    return this.props.songs.map(({ title, duration }) => {
+      return (
+        <div className="item" key={title}>
+          <div className="right floated content">
+            <button className="ui button primary">Select</button>
+          </div>
+          <div className="content">{title}</div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    console.log(this.props);
-    return (
-      <div>
-        <h1>SongList</h1>
-      </div>
-    );
+    // console.log(this.props.songs);
+    return <div>{this.renderList()}</div>;
   }
 }
 
